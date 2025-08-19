f5680412ffb4ec442b93f526c41a43098ebcb08f
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2025-08-19 15:03:36 +0530

    Add files via upload

---
+++ Transpose.java
@@ -0,0 +1,37 @@
+//Author:Vismaya Theresa Benny
+//java program to find transpose
+
+package jav;
+import java.util.Scanner;
+public class Transpose {
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in) ;
+			System.out.println("Enter size of matrix:");
+			int n=sc.nextInt();
+			int [] [] matrix=new int[n][n];
+			System.out.println("Enter elements:");
+			for(int i=0;i<n;i++) {
+				for(int j=0;j<n;j++) {
+					matrix[i][j]=sc.nextInt();
+				}
+			}
+			System.out.println("Matrix:");
+			for(int i=0;i<n;i++) {
+				for(int j=0;j<n;j++) {
+					System.out.print(matrix[i][j]+"\t");
+				}
+			System.out.println();
+			}
+			System.out.println();
+			System.out.println("Transpose:");
+			for(int i=0;i<n;i++) {
+				for(int j=0;j<n;j++) {
+					System.out.print(matrix[j][i]+"\t");
+				}
+			System.out.println();
+			}
+		}
+	
+}
+
+
